3967415d3391be7ff3ae24533e3a232d08f963c6
itayalasas <info@ayalait.com.uy>
2025-07-27 01:24:35 -0300

    Updated _layout.tsx

--- app/(admin-tabs)/_layout.tsx
+++ app/(admin-tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { BarChart3, Users, Settings, MapPin, Megaphone } from 'lucide-react-native';
+import { ChartBar as BarChart3, Users, Settings, MapPin, Megaphone } from 'lucide-react-native';
 
 export default function AdminTabLayout() {
   return (
