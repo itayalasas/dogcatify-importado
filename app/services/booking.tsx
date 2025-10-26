df2d5db6014553a1151e4a3a56650c18da60ffd8
itayalasas <info@ayalait.com.uy>
2025-10-26 18:00:20 -0300

    Updated booking.tsx

--- app/services/booking.tsx
+++ app/services/booking.tsx
@@ -615,7 +615,7 @@ export default function ServiceBooking() {
         </View>
       </ScrollView>
 
-      {/* Modal de Métodos de Pago */}
+      {/* Modal de Métodos de Pago - Actualizado */}
       <Modal
         visible={showPaymentMethodModal}
         transparent
