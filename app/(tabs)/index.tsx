f66b45ec6756ab84fb98b9a65d23e15378581d7e
itayalasas <info@ayalait.com.uy>
2025-11-26 13:50:34 +0000

    Updated package.json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import Constants from 'expo-constants';
 import PostCard from '../../components/PostCard';
 import PromotionCard from '../../components/PromotionCard';
 import { DottyAssistant } from '../../components/DottyAssistant';
+import { FloatingVoiceBot } from '../../components/FloatingVoiceBot';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { useAuth } from '../../contexts/AuthContext';
 import { NotificationPermissionPrompt } from '../../components/NotificationPermissionPrompt';
@@ -942,6 +943,8 @@ export default function Home() {
           onSkip={handleOnboardingComplete}
         />
       )}
+
+      <FloatingVoiceBot />
     </SafeAreaView>
   );
 }
